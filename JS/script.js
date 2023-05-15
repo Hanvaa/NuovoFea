c2b55dac57b548ffd426b4620e5928804c0b3704
Studente <Studente@Vallauri.Net>
2023-05-15 09:35:28 +0200

    Giusto mappa ma commentato perchè fea ha detto così

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,16 @@
  var map;
 
 $(document).ready(function() {
+
+    
+    /*
     //Definisco una mappa
     map = new ol.map(
         {
-            target:"map",/* l'id dell'oggetto html */
+            target:"map",// l'id dell'oggetto html //
             layers:[//gli indichiamo quali livelli deve avere questa mappa, gli dirempo che il primoi livello è la mappa globale
                 //Definisco il livello base(tile vuol dire esseri?)(/mappa globale completa)
-                new ol.layers.Tile({ source: new ol.source.OSM()}) //Sorgente di mappa globale, quella più grande
+                new ol.layer.Tile({ source: new ol.source.OSM()}) //Sorgente di mappa globale, quella più grande
             ],
             //Caratteristiche visive (zoom,centro, ...) della mappa visualizzata
             //Centro = dove la mappa si concentra,mi fa vedere prima l'europa, o america o spagna
@@ -17,6 +20,7 @@ $(document).ready(function() {
                 {zoom:4}
             )
         });
+        */
 
 
 });
